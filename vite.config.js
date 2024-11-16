73fda4e328d5bf5e7351b9cf9a75b7e9a68e4521
dilshad360 <dilshad360.dev@gmail.com>
2024-11-16 11:24:50 +0530

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://buildemea.connectemea.in/',
+  base: 'https://connectemea.github.io/BuildEmea/',
   server: {
     port: 3000,
   },
